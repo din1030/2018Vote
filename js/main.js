b57ec7e6f07ee625633feb8f74907aaae196106d
Din Cheng <din1030@gmail.com>
2018-11-21 05:36:13 +0800

    ver 1.0

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
-var vStamp = '<img class="position-relative" src="../img/vote-cursor.png">';
+var vStamp = '<img class="stamp position-relative" src="../img/vote-cursor.png">';
 var valid = false,
 	agree = null,
 	correct = false;
 $(document).ready(function() {
+	$('#share-bar').share({
+
+		// page title
+		pageTitle: '',
+
+		// page url
+		pageUrl: window.location.href,
+
+		// page description
+		pageDesc: '',
+
+		// or 'right'
+		position: 'left',
+
+		// or 'square'
+		theme: 'circle',
+
+		// enable/disable animation
+		animate: true,
+
+		// popup width
+		popupWidth: 640,
+
+		// popup height
+		popupHeight: 528,
+
+		// an array of social networking services
+		channels: ['facebook', 'twitter', 'linkedin'],
+
+		// trigger class
+		itemTriggerClass: 'js-share'
+
+	});
+
 	$('.vote-section').mouseenter(function(e) {
 		$('.vote-stamp').show();
 		$(this).on('click', function(e) {
@@ -18,8 +52,10 @@ $(document).ready(function() {
 			var leftColRight = $(leftColumn).offset().left + $(leftColumn).outerWidth();
 			if ((sLeft > leftColRight - 7) && (sLeft <= leftColRight)) {
 				// 中間無法辨認地帶
-				$('.validation').text('無法判斷');
-				// alert('無法判斷');
+				$('.validation').text('無效票').addClass('text-danger').removeClass('text-success');
+				var invalid = '<p class="text-danger">您的選票將被視為無效票，請您務必閱讀<a target="_blank" href="assets/vote-validation.pdf">中選會公告之規定</a>。</p>';
+				$('.result-hint').html(invalid);
+				$('.btn-reset').removeAttr('disabled');
 				return;
 			} else {
 				$.each(validArea, function(index, area) {
@@ -50,40 +86,51 @@ $(document).ready(function() {
 				if (valid) {
 					if (agree) {
 						$('.validation').text('有效同意票').removeClass('text-danger').addClass('text-success');
-						// alert('有效且正確');
 					} else {
 						$('.validation').text('有效不同意票').removeClass('text-danger').addClass('text-success');
-						// alert('有效但投錯惹');
 					}
 					if (correct) {
-						var thankyou = '謝謝您投下支持婚姻平權/性平教育的一票，期待台灣能夠擁抱多元的性/別族群，也讓我們的孩子學會尊重每個不同的個體。';
-						$('.result-hint').text(thankyou);
+						var thankyou = '<p class="text-success">謝謝您站在支持婚姻平權/性平同志教育的這一邊，期待台灣能夠擁抱多元的性/別族群，也讓我們的孩子學會尊重每個不同的個體。</p>';
+						$('.result-hint').html(thankyou);
 					} else {
-						var opps = '提醒您，本案為愛家三案之一，若您支持婚姻平權/性平教育，請您將本案改投「不同意票」，期許台灣能夠擁抱多元的性/別族群，也讓我們的孩子學會尊重每個不同的個體。';
-						$('.result-hint').text(opps);
+						var id = $('body').attr('id');
+						var opps = '';
+						if (id == "vote14" || id == "vote15") {
+							opps = '<p class="text-danger">提醒您，本案為同志平權兩案之一（兩好），若您支持婚姻平權/性平教育，請您將本案改投<strong>「同意票」</strong>，期許台灣能夠擁抱多元的性/別族群，也讓我們的孩子學會尊重每個不同的個體。</p>';
+						} else {
+							opps = '<p class="text-danger">提醒您，本案為愛（礙）家三案之一（三壞），若您支持婚姻平權/性平教育，請您將本案改投<strong>「不同意票」</strong>，期許台灣能夠擁抱多元的性/別族群，也讓我們的孩子學會尊重每個不同的個體。</p>';
+						}
+						$('.result-hint').html(opps);
 					}
+					$('.btn-reset').removeAttr('disabled');
 				} else {
-					$('.result-hint').empty();
-					$('.validation').text('無法判斷').addClass('text-danger').removeClass('text-success');
-					// alert('無法判斷');
+					$('.validation').text('無效票').addClass('text-danger').removeClass('text-success');
+					var invalid = '<p class="text-danger">您的選票將被視為無效票，請您務必閱讀<a target="_blank" href="assets/vote-validation.pdf">中選會公告之規定</a>。</p>';
+					$('.result-hint').html(invalid);
+					$('.btn-reset').removeAttr('disabled');
 				}
 			}
 		});
 	});
+	// 離開投票區隱藏選舉章
 	$('.vote-section').mouseleave(function(e) {
 		$('.vote-stamp').hide();
 		$(this).off('click');
 	});
+	// 選舉章隨滑鼠移動
 	$('.vote-section').mousemove(function(e) {
-		// console.log(e);
-		// if ($(e.target).parents(".vote-section").length != 0) {
 		$('.vote-stamp').offset({
 			left: e.pageX - 25,
 			top: e.pageY - 25
 		});
-		// } else {
-		// 	console.log('out');
-		// 	$('.vote-stamp').hide();
-		// }
+	});
+	$('.btn-reset').click(function(e) {
+		valid = false;
+		agree = null;
+		correct = false;
+		$('img.stamp').remove();
+		$('.btn-reset').attr('disabled', '');
+		$('.validation').text('（尚未投票）').removeClass('text-danger').removeClass('text-success');
+		$('.result-hint').empty();
 	});
 });
